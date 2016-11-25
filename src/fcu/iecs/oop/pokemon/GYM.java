4aaf09d52a20cffcbdf99c10a66957d4fc94a7ba
yiwei <yiwei@yiwei-PC>
2016-11-25 19:13:26 +0800

    :)

---
+++ src/fcu/iecs/oop/pokemon/GYM.java
@@ -0,0 +1,21 @@
+package fcu.iecs.oop.pokemon;
+import java.util.Random;
+
+public class GYM {
+	
+	public static void fight(Pokemon p1, Pokemon p2){
+		Random ran = new Random();
+		int who =  ran.nextInt(2);
+		System.out.println(who);
+		if(who == 1){
+			System.out.println("Winner is : "+p1.getName());
+			p1.setCp(p1.getCp()+500);
+			System.out.println(p1.getCp());
+		}
+		else{
+			System.out.println("Winner is : "+p2.getName());
+			p2.setCp(p2.getCp()+500);
+			System.out.println(p2.getCp());
+		}
+	}
+}
---
+++ src/fcu/iecs/oop/pokemon/Pokemon.java
@@ -0,0 +1,29 @@
+package fcu.iecs.oop.pokemon;
+
+public class Pokemon {
+	private String name;
+	private int cp;
+	
+	
+	public Pokemon(String name,int cp) {
+		super();
+		this.name = name;
+		this.cp = cp;
+	}
+	
+	public int getCp() {
+		return cp;
+	}
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+	public  String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
